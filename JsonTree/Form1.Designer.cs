5c054d8ecf715f566d57396f5b25ab2b444d4c51
barnadenes <nyuk1007>
2019-11-03 19:40:41 +0100

    tobecontinued

---
+++ JsonTree/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace JsonTree
+{
+    partial class JSONTree
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openJSONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openJSONToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 26);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openJSONToolStripMenuItem
+            // 
+            this.openJSONToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.openJSONToolStripMenuItem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.openJSONToolStripMenuItem.Font = new System.Drawing.Font("Sitka Small", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.openJSONToolStripMenuItem.Name = "openJSONToolStripMenuItem";
+            this.openJSONToolStripMenuItem.Size = new System.Drawing.Size(88, 22);
+            this.openJSONToolStripMenuItem.Text = "Open JSON";
+            this.openJSONToolStripMenuItem.Click += new System.EventHandler(this.openJSONToolStripMenuItem_Click);
+            // 
+            // treeView
+            // 
+            this.treeView.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.treeView.Location = new System.Drawing.Point(241, 29);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(301, 452);
+            this.treeView.TabIndex = 1;
+            // 
+            // JSONTree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 484);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Name = "JSONTree";
+            this.Text = "JsonTree";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openJSONToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView;
+    }
+}
+
---
+++ JsonTree/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace JsonTree
+{
+    public partial class JSONTree : Form
+    {
+        private OpenFileDialog dialog;
+
+        public JSONTree()
+        {
+            InitializeComponent();
+        }
+
+        private void openJSONToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.dialog = new OpenFileDialog();
+            dialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            dialog.ShowDialog();
+
+            string path = dialog.FileName;
+            string extension = Path.GetExtension(path);
+
+            if (extension != ".json")
+            {
+                MessageBox.Show("Not a JSON file");
+            }
+            else
+            {               
+                using (var reader = new StreamReader(path))
+                using (var jsonReader = new JsonTextReader(reader))
+                {
+                    var root = JToken.Load(jsonReader);
+                    DisplayTreeView(root, Path.GetFileNameWithoutExtension(path));                 
+                }               
+            }
+        }
+
+        private void DisplayTreeView(JToken root, string rootName)
+        {
+            treeView.BeginUpdate();
+            try
+            {
+                treeView.Nodes.Clear();
+                var tNode = treeView.Nodes[treeView.Nodes.Add(new TreeNode(rootName))];
+                tNode.Tag = root;
+
+                AddNode(root, tNode);              
+            }
+            finally
+            {
+                treeView.EndUpdate();
+            }
+        }
+
+        private void AddNode(JToken token, TreeNode inTreeNode)
+        {
+            if (token == null)
+                return;
+            if (token is JValue)
+            {
+                var childNode = inTreeNode.Nodes[inTreeNode.Nodes.Add(new TreeNode(token.ToString()))];
+                childNode.Tag = token;
+            }
+            else if (token is JObject)
+            {
+                var obj = (JObject)token;
+                foreach (var property in obj.Properties())
+                {
+                    var childNode = inTreeNode.Nodes[inTreeNode.Nodes.Add(new TreeNode(property.Name))];
+                    childNode.Tag = property;
+                    AddNode(property.Value, childNode);
+                }
+            }
+            else if (token is JArray)
+            {
+                var array = (JArray)token;
+                for (int i = 0; i < array.Count; i++)
+                {
+                    var childNode = inTreeNode.Nodes[inTreeNode.Nodes.Add(new TreeNode(i.ToString()))];
+                    childNode.Tag = array[i];
+                    AddNode(array[i], childNode);
+                }
+            }
+        }
+    }
+}
